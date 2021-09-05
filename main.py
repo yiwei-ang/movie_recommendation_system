e14076f0413d521d6bdbc85e8c3c4933e04f72c5
mosesyiwei <mosesyiwei@gmail.com>
2021-09-05 23:25:52 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+import os
+import json
+import pandas as pd
+import numpy as np
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+PROJECT_DIR = 'C:\\Users\\user\\Documents\\movie_recommendation_system\\'
+
+# Import CountVectorizer and create the count matrix
+def load_model(model_csv):
+    count = CountVectorizer(stop_words='english')
+    count_matrix = count.fit_transform(model_csv['soup'])
+
+    # Compute the Cosine Similarity matrix based on the count_matrix
+    cosine_sim = cosine_similarity(count_matrix, count_matrix)
+
+    # Reset index of our main DataFrame and construct reverse mapping as before
+    model_csv = model_csv.reset_index()
+    indices = pd.Series(model_csv.index, index=model_csv['original_title'])
+    return model_csv, cosine_sim, indices
+
+# Function that takes in movie title as input and outputs most similar movies
+def get_recommendations(title, model_csv, cosine_sim, indices):
+    # Get the index of the movie that matches the title
+    idx = indices[title]
+
+    # Get the pairwsie similarity scores of all movies with that movie
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    # Sort the movies based on the similarity scores
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # Get the scores of the 10 most similar movies
+    sim_scores = sim_scores[1:11]
+
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores]
+
+    # Return the top 10 most similar movies
+    return model_csv['original_title'].iloc[movie_indices]
+
+if __name__ == "__main__":
+    print('Loading model...')
+    model_csv, cosine_sim, indices = load_model(pd.read_csv(PROJECT_DIR + '\\model\\model.csv'))
+    print('Done loading model...')
+    # An input is requested and stored in a variable
+    input_title = input("Enter a movie title: ")
+    print(get_recommendations(input_title, model_csv=model_csv, cosine_sim=cosine_sim, indices=indices))
---
+++ model/model.py
@@ -0,0 +1,59 @@
+import os
+import json
+import pandas as pd
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+PROJECT_DIR = 'C:\\Users\\user\\Documents\\movie_recommendation_system\\'
+df_movie   = pd.read_csv(PROJECT_DIR + '\\dataset\\tmdb_5000_movies.csv')
+df_credits = pd.read_csv(PROJECT_DIR + '\\dataset\\tmdb_5000_credits.csv')
+
+df_credits.rename(columns={'movie_id': 'id'}, inplace=True)
+df_movie = df_movie.merge(df_credits, on='id')
+df_movie.drop('id', inplace=True, axis=1)
+
+# Features extraction from JSON
+casts = ['Editor', 'Producer', 'Writer', 'Director']
+for cast in casts:
+    df_movie[cast] = df_movie['crew'].apply(lambda x:list({x.get('name') for x in json.loads(x) if x.get('job') == cast}))
+
+features = ['keywords', 'production_companies', 'production_countries', 'genres']
+for feature in features:
+    df_movie[feature] = df_movie[feature].apply(lambda x:[i.get('name') for i in json.loads(x)])
+
+df_movie_numerical = df_movie.select_dtypes('number')
+df_movie_categorical = df_movie.select_dtypes('object')[['original_title', 'keywords', 'genres', 'original_language', 'production_companies', 'cast', 'Director', 'Producer', 'Writer', 'Editor']]
+df_movie = pd.concat([df_movie_numerical, df_movie_categorical], axis = 1)
+
+# Features extraction using most frequent appearance given samples.
+features_top3 = ['keywords', 'genres', 'cast']
+for feature in features_top3:
+    unique_ = [item for sublist in df_movie[feature].tolist() for item in sublist]
+    unique_ = pd.DataFrame({feature: unique_}).groupby(feature).size()
+    df_movie[feature] = df_movie[feature].apply(lambda x:unique_[(unique_.index).isin(pd.Series(x, dtype='object'))].sort_values(ascending = False)[:3].index.tolist())
+
+
+# Function to convert all strings to lower case and strip names of spaces
+def clean_data(x):
+    if isinstance(x, list):
+        return [str.lower(i.replace(" ", "")) for i in x]
+    else:
+        # Check if director exists. If not, return empty string
+        if isinstance(x, str):
+            return str.lower(x.replace(" ", ""))
+        else:
+            return ''
+
+
+# Apply clean_data function to your features.
+features = ['keywords', 'genres', 'cast', 'Director']
+
+for feature in features:
+    df_movie[feature] = df_movie[feature].apply(clean_data)
+
+def create_soup(x):
+    return ' '.join(x['keywords']) + ' ' + ' '.join(x['cast']) + ' ' + ' '.join(x['Director']) + ' ' + ' '.join(x['genres'])
+
+df_movie['soup'] = df_movie.apply(create_soup,axis=1)
+df_movie.to_csv(PROJECT_DIR + '\\model\\model.csv', index=False)
